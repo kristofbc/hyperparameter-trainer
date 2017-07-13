9270aed4ea3573fad6e6ea3ed89c9a4733363c36
Kristof Boucher Charbonneau <kristof.b.c@me.com>
2017-07-13 13:32:34 +0900

    Adding the setup file to make the repo a library

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='hyperparameter-trainer',
+    version='1.0.0',
+    description='Try different hyperparameters for a neural network, easily.',
+    long_description=long_description,
+    url='https://github.jp.honda-ri.com/kcharbonneau/chainer-hyperparameter-trainer',
+    license='MIT',
+    author='Kristof Boucher Charbonneau',
+    author_email='k.charbonneau@jp.honda-ri.com',
+    packages=['hyperparameter-trainer'],
+    entry_points={ },
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+    ],
+)
